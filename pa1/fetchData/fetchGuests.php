3c795a61eb6aa37970b02e0d8b0c3930bb5ba1cd
Walden <walden980302@outlook.com>
2024-02-28 20:09:17 -0500

    update index and fetchGuest
    1. use extra php file to avoid code redundancy in the index.php 2. Add "age limit" code from original index.php into fetchGuest.php

--- pa1/fetchData/fetchGuests.php
+++ pa1/fetchData/fetchGuests.php
@@ -1,5 +1,20 @@
 <?php
 require 'connectDB.php'; // Include database connection
+
+// we want to check if the submit button has been clicked (in our case, it is named Query)
+if(isset($_POST['submitted']))
+{
+    // set age limit to whatever input we get
+    // ideally, we should do more validation to check for numbers, etc. 
+$ageLimit = $_POST["inputAge"]; 
+}
+else
+{
+    // if the button was not clicked, we can simply set age limit to 0 
+    // in this case, we will return everything
+    $ageLimit = 0;
+}
+
 try{
     // prepare statement for executions. This part needs to change for every query
     $stmt = $conn->prepare("SELECT first_name, last_name FROM guests where age>=$ageLimit");
--- pa1/index.php
+++ pa1/index.php
@@ -49,10 +49,26 @@
     </form>
 
     <div class="container">
-        <h1>NAV VIA BUTTONS</h1>
-        
-
     <?php
+    // generic table builder. It will automatically build table data rows irrespective of result
+    class TableRows extends RecursiveIteratorIterator {
+        function __construct($it) {
+            parent::__construct($it, self::LEAVES_ONLY);
+        }
+
+        function current() {
+            return "<td style='text-align:center'>" . parent::current(). "</td>";
+        }
+
+        function beginChildren() {
+            echo "<tr>";
+        }
+
+        function endChildren() {
+            echo "</tr>" . "\n";
+        }
+    }
+
     $viewMode = ''; // Default view is empty
 
     // Detect which button was clicked
@@ -69,6 +85,8 @@
     switch ($viewMode) {
         case 'movies':
             // Code to display movie information
+            include 'fetchData/fetchMovies.php';
+            include 'views/moviesView.php';
             break;
         case 'users':
             // Code to display user information
@@ -77,80 +95,8 @@
             // Code to display actor information
             break;
         case 'guests':
-            // we want to check if the submit button has been clicked (in our case, it is named Query)
-            if(isset($_POST['submitted']))
-            {
-                // set age limit to whatever input we get
-                // ideally, we should do more validation to check for numbers, etc. 
-            $ageLimit = $_POST["inputAge"]; 
-            }
-            else
-            {
-                // if the button was not clicked, we can simply set age limit to 0 
-                // in this case, we will return everything
-                $ageLimit = 0;
-            }
-
-            // we will now create a table from PHP side 
-            echo "<table class='table table-md table-bordered'>";
-            echo "<thead class='thead-dark' style='text-align: center'>";
-
-            // initialize table headers
-            // YOU WILL NEED TO CHANGE THIS DEPENDING ON TABLE YOU QUERY OR THE COLUMNS YOU RETURN
-            echo "<tr><th class='col-md-2'>Firstname</th><th class='col-md-2'>Lastname</th></tr></thead>";
-
-            // generic table builder. It will automatically build table data rows irrespective of result
-            class TableRows extends RecursiveIteratorIterator {
-                function __construct($it) {
-                    parent::__construct($it, self::LEAVES_ONLY);
-                }
-
-                function current() {
-                    return "<td style='text-align:center'>" . parent::current(). "</td>";
-                }
-
-                function beginChildren() {
-                    echo "<tr>";
-                }
-
-                function endChildren() {
-                    echo "</tr>" . "\n";
-                }
-            }
-
-            // SQL CONNECTIONS
-            $servername = "localhost";
-            $username = "root";
-            $password = "";
-            $dbname = "COSI127b";
-
-            try {
-                // We will use PDO to connect to MySQL DB. This part need not be 
-                // replicated if we are having multiple queries. 
-                // initialize connection and set attributes for errors/exceptions
-                $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-                $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-                // prepare statement for executions. This part needs to change for every query
-                $stmt = $conn->prepare("SELECT first_name, last_name FROM guests where age>=$ageLimit");
-
-                // execute statement
-                $stmt->execute();
-
-                // set the resulting array to associative. 
-                $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
-
-                // for each row that we fetched, use the iterator to build a table row on front-end
-                foreach(new TableRows(new RecursiveArrayIterator($stmt->fetchAll())) as $k=>$v) {
-                    echo $v;
-                }
-            }
-            catch(PDOException $e) {
-                echo "Error: " . $e->getMessage();
-            }
-            echo "</table>";
-            // destroy our connection
-            // $conn = null;
+            include 'fetchData/fetchGuests.php';
+            include 'views/guestsView.php';
             break;
         default:
             // Default content, perhaps a welcome message or introduction
