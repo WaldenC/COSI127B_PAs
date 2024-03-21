72c6119efe3c99fea31ee9b3482721e498375b84
Walden <walden980302@outlook.com>
2024-03-21 15:59:56 -0400

    Update Query 14 table's name

--- pa1/views/query14View.php
+++ pa1/views/query14View.php
@@ -1,6 +1,6 @@
 <?php
 
-echo "<h1>The actors who have played a role in both “Marvel” and “Warner Bros” productions</h1>";
+echo "<h1>The top 5 movies with the highest number of people playing a role in that movie</h1>";
 echo "<table class='table table-md table-bordered'>";
 echo "<thead class='thead-dark' style='text-align: center'>";
 
